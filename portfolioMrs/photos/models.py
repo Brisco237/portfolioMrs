b204488570b3f06f13c3cf5c7ff9185fe352fce1
kamdem Brice <kamdembrice770@gmail.com>
2025-10-15 20:42:19 +0100

    creation du model photos

--- portfolioMrs/photos/models.py
+++ portfolioMrs/photos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Photos(models.Model):
+    image = models.ImageField(upload_to='media/photos')
+    title = models.TextField(max_length=1000)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    technique = models.TextField(max_length=1000)
+    size = models.TextField(max_length=1000)
+    description = models.TextField(max_length=1000)
