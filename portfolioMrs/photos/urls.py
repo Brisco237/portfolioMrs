811e752adf3c75fb11f7c9e829fa63b21e69fead
kamdem Brice <kamdembrice770@gmail.com>
2025-10-16 23:37:06 +0100

    add about page

--- portfolioMrs/photos/urls.py
+++ portfolioMrs/photos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import store
+from .views import store, about
 
 urlpatterns = [
     path('store/', store, name="store"),
+    path('about/', about, name="about"),
 ]
--- portfolioMrs/photos/views.py
+++ portfolioMrs/photos/views.py
@@ -4,4 +4,8 @@ from .models import Photos
 # Create your views here.
 def store(request):
     photos = Photos.objects.all()
-    return render(request, 'photos/store.html', {'photos' : photos})
+    total  = Photos.objects.count()
+    return render(request, 'photos/store.html', {'photos' : photos, 'total' : total})
+
+def about(request):
+    return render(request, 'photos/about.html')
