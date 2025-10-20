803fb20be0731018892ae92fb8af9b733a40de76
kamdem Brice <kamdembrice770@gmail.com>
2025-10-20 19:49:29 +0100

    ajout des dessins originaux

--- portfolioMrs/photos/admin.py
+++ portfolioMrs/photos/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Photos
+from .models import Photos,Categorie
 
 # Register your models here.
+admin.site.register(Categorie)
 admin.site.register(Photos)
 
 admin.site.site_title = "Espace Administrateur"
