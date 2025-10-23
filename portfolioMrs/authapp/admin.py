946d8c627c10b72975d50284ea94e5d033a596b6
kamdem Brice <kamdembrice770@gmail.com>
2025-10-23 03:46:58 +0100

    ajouter mes users a l'admin

--- portfolioMrs/authapp/admin.py
+++ portfolioMrs/authapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+class CustomUser(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_superuser', 'date_joined')
+
+admin.site.register(User, CustomUser)
